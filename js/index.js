3e8ab1a8a2f2faf7aecea0377937c091c9298c0b
Pasha Belugin <pashaigood@gmail.com>
2013-04-21 02:59:58 +0400

    new namespace

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 include(
     'jquery.index',
-    'jquery.ui.index',
+    // 'jquery.ui.index',
 	'test.ClassStructure',
 	function() {
 		var popa = new test.ClassStructure();
-		var popa2 = new test.ClassStructure();
+		// popa.toString = function() {
+		    // // console.profile('test.ClassStructure');
+		    // return false;
+		// }
+// 		
+		// popa.valueOf = function() {
+		    // console.log('run why');
+		    // return "dad";
+		// }
 		
-		popa.addChild(popa2);
-		popa.test1('out');
-		popa.superTest();
+		// var popa2 = new test.ClassStructure();
+		
+		// popa.addChild(popa2);
+		// popa.test1('out');
+		// popa.superTest();
+	    console.log(test.ClassStructure.POPA);
+	    console.log(popa);
 	}
 );
